337deaf1f73b001ec24a1b383f4605dfc2243a23
“gouliguojiashengsiyi” <gouliguojiashengsiyi@sjtu.edu.cn>
2023-09-25 23:55:54 +0800

    LinkList.cpp update 0925

--- LinkList.cpp
+++ LinkList.cpp
@@ -1,2 +1,62 @@
 #include <iostream>
 #include "LinkList.hpp"
+
+template<class elemType>
+LinkList<elemType>::LinkList()
+{
+    head = new node();
+}
+
+template<class elemType>
+void LinkList<elemType>::clear()
+{
+    
+}
+
+template<class elemType>
+void LinkList<elemType>::insert(int i, const elemType &x)
+{
+    
+}
+
+template<class elemType>
+void LinkList<elemType>::insert(elemType a[], int n)
+{
+    
+}
+
+template<class elemType>
+int LinkList<elemType>::length() const
+{
+    
+}
+
+template<class elemType>
+void LinkList<elemType>::remove(int i)
+{
+    
+}
+
+template<class elemType>
+void LinkList<elemType>::reverse()
+{
+    
+}
+
+template<class elemType>
+int LinkList<elemType>::search(const elemType &x) const
+{
+    
+}
+
+template<class elemType>
+void LinkList<elemType>::traverse() const
+{
+    
+}
+
+template<class elemType>
+elemType LinkList<elemType>::visit(int i) const
+{
+    
+}
